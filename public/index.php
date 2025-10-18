4f180fcd733e0dbf40127b4454f5646d27a5eb5e
Laracoper <113640131+Laracoper@users.noreply.github.com>
2025-10-18 19:26:39 +0300

    add new files

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
+// error_reporting(0);
+
 use App\Calculator;
 
 
@@ -18,25 +20,31 @@ $calculator = new Calculator();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
+    <link rel="shortcut icon" href="img/Frame-17svg.svg" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <h1>Простой калькулятор</h1>
-    <form action="" method="post">
-        <input type="text" name="a" placeholder="первое число">
-        <select name="znak">
-            <option value="">выбрать действие</option>
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="/">/</option>
-            <option value="*">*</option>
-        </select>
-        <input type="text" name="b" placeholder="второе число">
-        <button type="submit">посчитать</button>
-    </form>
-    <div class="result">
-        <p>результат: <span class="res"><?= $calculator->calc() ?></span></p>
+    <div class="container">
+        <h1>Простой калькулятор</h1>
+        <div class="calc">
+            <form action="" method="post" class="form">
+                <input type="number" name="a" placeholder="первое число" class="field">
+                <select name="znak" class="field">
+                    <option value="">выбрать действие</option>
+                    <option value="+">+</option>
+                    <option value="-">-</option>
+                    <option value="/">/</option>
+                    <option value="*">*</option>
+                </select>
+                <input type="number" name="b" placeholder="второе число" class="field">
+                <button type="submit" class="btn">посчитать</button>
+            </form>
+            <div class="result">
+                <p>результат: <span class="res"><?= $calculator->calc() ?></span></p>
+
+            </div>
+        </div>
 
     </div>
 </body>
